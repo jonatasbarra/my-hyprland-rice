97efc447586f7dcf3ac2903fd4605938951f1189
jonatasbarra <email.jonatas@gmail.com>
2026-05-26 22:23:04 -0400

    feat(zsh): add Android SDK to PATH

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,3 +4,9 @@ export VISUAL="$EDITOR"
 export PAGER="less"
 export LESS="-R --use-color"
 eval "$(mise activate zsh)"
+
+# Android SDK
+export ANDROID_HOME="$HOME/Android/Sdk"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
